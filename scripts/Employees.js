c02eae759afac26862fa7fea93f7ebd537c49095
Will Hedges <wshedges@outlook.com>
2022-11-30 11:40:50 -0600

    employee products sold alert working * click event handler added to Employees.js

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,7 @@
-import { getEmployees } from "./database.js";
+import { getEmployees, getOrders } from "./database.js";
 
 const employees = getEmployees();
+const orders = getOrders();
 
 export const Employees = () => {
   let html = "<ul>";
@@ -13,3 +14,33 @@ export const Employees = () => {
 
   return html;
 };
+
+// create a new event listener to show how many products were sold by each person
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  // go through all the employees IFF the clicked item is an employee
+  if (itemClicked.id.startsWith("employee")) {
+    // get the employee primary key
+    const [, employeeId] = itemClicked.id.split("--");
+    // use the employee PK to get the employee name
+    let employeeName = "";
+    for (const employee of employees) {
+      if (employee.id == employeeId) {
+        employeeName = employee.name;
+      }
+    }
+
+    let totalOrders = 0;
+    /* 
+      go through all the orders, and increment the total orders
+      when you find one that matches the employee
+    */
+    for (const order of orders) {
+      if (order.employeeId == employeeId) {
+        totalOrders++;
+      }
+    }
+    // display an alert with the employees name and how many products they sold
+    window.alert(`${employeeName} sold ${totalOrders} products`);
+  }
+});
